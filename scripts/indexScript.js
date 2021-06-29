7c8b092f8012b566232bbe782e2b37ba31106397
Adrian_vSk <agvsmail@gmail.com>
2021-06-29 11:17:04 +0100

    Read More button added to featured news articles on Home Page.

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -4,3 +4,21 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+const parentContainer = document.querySelector('.news-text');
+
+parentContainer.addEventListener('click', (e) => {
+
+    const current = e.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = e.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
