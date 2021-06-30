f58d16964f314af061d572dfea01bb9909e3e520
Adrian_vSk <agvsmail@gmail.com>
2021-06-30 17:27:57 +0100

    Terms of use and Privacy Policy added.

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -22,3 +22,21 @@ parentContainer.addEventListener('click', (e) => {
     current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
 
 })
+
+const parentContainer2 = document.querySelector('.news-text2');
+
+parentContainer2.addEventListener('click', (e) => {
+
+    const current = e.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = e.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
