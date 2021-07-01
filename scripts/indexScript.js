982a5ade8ed74a588665f039dceca46d7c2e1d78
Adrian_vSk <agvsmail@gmail.com>
2021-07-01 14:34:18 +0100

    Added to Featured News and Rumours. Also added safety tags to all pages.

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -5,7 +5,7 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-const parentContainer = document.querySelector('.news-text');
+const parentContainer = document.querySelector('.story-1');
 
 parentContainer.addEventListener('click', (e) => {
 
@@ -23,7 +23,7 @@ parentContainer.addEventListener('click', (e) => {
 
 })
 
-const parentContainer2 = document.querySelector('.news-text2');
+const parentContainer2 = document.querySelector('.story-2');
 
 parentContainer2.addEventListener('click', (e) => {
 
@@ -41,7 +41,7 @@ parentContainer2.addEventListener('click', (e) => {
 
 })
 
-const parentContainer3 = document.querySelector('.news-text3');
+const parentContainer3 = document.querySelector('.story-3');
 
 parentContainer3.addEventListener('click', (e) => {
 
@@ -59,9 +59,27 @@ parentContainer3.addEventListener('click', (e) => {
 
 })
 
-const parentContainer4 = document.querySelector('.news-text4');
+const parentContainer4 = document.querySelector('.story-4');
 
 parentContainer4.addEventListener('click', (e) => {
+
+    const current = e.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = e.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
+
+const parentContainer5 = document.querySelector('.story-5');
+
+parentContainer5.addEventListener('click', (e) => {
 
     const current = e.target;
 
